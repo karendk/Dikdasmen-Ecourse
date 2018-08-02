a9bf87bb6bea0e77aebade784cd9f2b9a2d26944
karendk <karenmakus@gmail.com>
2018-08-03 06:31:25 +0700

    merapihkan

--- application/models/Model_admin.php
+++ application/models/Model_admin.php
@@ -2,14 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_admin extends CI_Model {
-
 	var $table = 'user_detail';
 	var $column_order = array('username','kelas',null); //set column field database for datatable orderable
 	var $column_search = array('username','kelas'); //set column field database for datatable searchable just firstname , lastname , address are searchable
 	var $order = array('username'=>'desc'); // default order 
 
-	public function __construct()
-	{
+	public function __construct(){
 		parent::__construct();
 		$this->load->database();
 	}
@@ -18,47 +16,35 @@ class Model_admin extends CI_Model {
 		$this->table=$table;
 	}
 
-	private function _get_datatables_query()
-	{
-		
+	private function _get_datatables_query(){
 		$this->db->from($this->table);
-
 		$i = 0;
-	
-		foreach ($this->column_search as $item) // loop column 
-		{
-			if($_POST['search']['value']) // if datatable send POST for search
-			{
-				
-				if($i===0) // first loop
-				{
+		foreach ($this->column_search as $item){ // loop column 		
+			if($_POST['search']['value']){ // if datatable send POST for search		
+				if($i===0){ // first loop
 					$this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
 					$this->db->like($item, $_POST['search']['value']);
 				}
-				else
-				{
+				else{
 					$this->db->or_like($item, $_POST['search']['value']);
 				}
-
-				if(count($this->column_search) - 1 == $i) //last loop
+				if(count($this->column_search) - 1 == $i){ //last loop
 					$this->db->group_end(); //close bracket
+				}
 			}
 			$i++;
 		}
 		
-		if(isset($_POST['order'])) // here order processing
-		{
+		if(isset($_POST['order'])){ // here order processing
 			$this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
 		} 
-		else if(isset($this->order))
-		{
+		else if(isset($this->order))	{
 			$order = $this->order;
 			$this->db->order_by(key($order), $order[key($order)]);
 		}
 	}
 
-	function get_datatables()
-	{
+	function get_datatables(){
 		$this->_get_datatables_query();
 		if($_POST['length'] != -1)
 		$this->db->limit($_POST['length'], $_POST['start']);
@@ -66,21 +52,18 @@ class Model_admin extends CI_Model {
 		return $query->result();
 	}
 
-	function count_filtered()
-	{
+	function count_filtered(){
 		$this->_get_datatables_query();
 		$query = $this->db->get();
 		return $query->num_rows();
 	}
 
-	public function count_all()
-	{
+	public function count_all(){
 		$this->db->from($this->table);
 		return $this->db->count_all_results();
 	}
 
-	public function get_by_id($id)
-	{
+	public function get_by_id($id){
 		$this->db->from($this->table);
 		$this->db->where('id',$id);
 		$query = $this->db->get();
@@ -88,23 +71,18 @@ class Model_admin extends CI_Model {
 		return $query->row();
 	}
 
-	public function save($data)
-	{
+	public function save($data){
 		$this->db->insert($this->table, $data);
 		return $this->db->insert_id();
 	}
 
-	public function update($where, $data)
-	{
+	public function update($where, $data){
 		$this->db->update($this->table, $data, $where);
 		return $this->db->affected_rows();
 	}
 
-	public function delete_by_id($id)
-	{
+	public function delete_by_id($id){
 		$this->db->where('id', $id);
 		$this->db->delete($this->table);
 	}
-
-
 }
